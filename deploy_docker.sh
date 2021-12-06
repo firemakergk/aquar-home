67f2f4e242247ad4c77824e83e9c309e2fb52dec
gaolifeng <sklxz@qq.com>
2021-12-06 11:09:50 +0800

    docker镜像迁移至alpine由于没有bash以及shopt命令导致失败，暂时搁置

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -8,7 +8,7 @@ git pull
 cd /opt/aquar/src/docker-compose/
 docker-compose stop
 cd /opt/aquar/src/aquar-home
-docker image build -t finetu/aquarhome:latest .
+docker image build --no-cache -t finetu/aquarhome:latest .
 docker images
 cmd=$1
 if [ "$cmd" == "push" ]; then
