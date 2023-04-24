ee1a452e9b68dbb901a58845148d79299881d64f
firemakergk <sklxz@qq.com>
2023-04-24 07:35:35 +0000

    记录todo

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -12,6 +12,7 @@ DOCKER_BUILDKIT=0 docker image build --no-cache -t finetu/aquarhome:latest .
 docker images
 cmd=$1
 if [ "$cmd" == "push" ]; then
+    # docker tag 2f3890615562 finetu/aquarhome:1.1.9
     docker login --username=finetu -p $DOCKER_HUB_ACCESS_TOKEN
     docker push finetu/aquarhome:latest
     echo "最新镜像已推送至docker hub"
